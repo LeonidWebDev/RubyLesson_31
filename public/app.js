689de8eb7d49ef09dd805e2683631cb86847120d
LeonidWebDev <55695468+LeonidWebDev@users.noreply.github.com>
2020-08-15 19:13:11 +0300

    moveJSInitToLauout

--- public/app.js
+++ public/app.js
@@ -22,6 +22,7 @@ function add_to_cart(id) {
     window.localStorage.setItem(key, x)
 
     update_orders_input()
+    update_orders_button()
 }
 
 function cart_get_number_of_iems() {
@@ -45,6 +46,12 @@ function update_orders_input() {
 
     let orders = cart_get_orders() // get string from cart
     $('#orders_input').val(orders)
+}
+
+function update_orders_button() {
+
+    let text = 'Cart (' + cart_get_number_of_iems() + ')'
+    $('#orders_button').val(text)
 }
 
 function cart_get_orders() {
